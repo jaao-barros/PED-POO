9af022cc15bc1c7eda25728c257889ef68bc1225
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-07-11 00:11:07 -0300

    feat: Criar métodos para manipular estado do programa
    Criar métodos para adicionar, remover e buscar objetos específicos num conjunto de objetos

--- src/model/Model.java
+++ src/model/Model.java
@@ -1,29 +1,29 @@
 package model;
 
-import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 import view.Observer;
 
 public class Model {
     private static Model instancia;
-    private final List<Coordenacao> coordenacoes;
-    private final List<Curso> cursos;
-    private final List<Disciplina> disciplinas;
-    private final List<PlanoDeEnsino> planosDeEnsino;
-    private final List<Professor> professores;
-    private final List<UnidadeAcademica> unidadesAcademicas;
+    private final Map<Integer, Coordenacao> coordenacoes;
+    private final Map<Integer, Curso> cursos;
+    private final Map<Integer, Disciplina> disciplinas;
+    private final Map<Integer, PlanoDeEnsino> planosDeEnsino;
+    private final Map<Integer, Professor> professores;
+    private final Map<Integer, UnidadeAcademica> unidadesAcademicas;
     private final Set<Observer> observers;
 
     private Model() {
-        coordenacoes = new ArrayList<>();
-        cursos = new ArrayList<>();
-        disciplinas = new ArrayList<>();
-        planosDeEnsino = new ArrayList<>();
-        professores = new ArrayList<>();
-        unidadesAcademicas = new ArrayList<>();
+        coordenacoes = new HashMap<>();
+        cursos = new HashMap<>();
+        disciplinas = new HashMap<>();
+        planosDeEnsino = new HashMap<>();
+        professores = new HashMap<>();
+        unidadesAcademicas = new HashMap<>();
         observers = new HashSet<>();
     }
 
@@ -35,32 +35,176 @@ public class Model {
         return instancia;
     }
 
-    public List<Coordenacao> getCoordenacoes() {
+    public Map<Integer, Coordenacao> getCoordenacoes() {
         return coordenacoes;
     }
 
-    public List<Curso> getCursos() {
+    public Map<Integer, Curso> getCursos() {
         return cursos;
     }
 
-    public List<Disciplina> getDisciplinas() {
+    public Map<Integer, Disciplina> getDisciplinas() {
         return disciplinas;
     }
 
-    public List<PlanoDeEnsino> getPlanosDeEnsino() {
+    public Map<Integer, PlanoDeEnsino> getPlanosDeEnsino() {
         return planosDeEnsino;
     }
 
-    public List<Professor> getProfessores() {
+    public Map<Integer, Professor> getProfessores() {
         return professores;
     }
 
-    public List<UnidadeAcademica> getUnidadesAcademicas() {
+    public Map<Integer, UnidadeAcademica> getUnidadesAcademicas() {
         return unidadesAcademicas;
     }
 
     public Set<Observer> getObservers() {
         return observers;
+    }
+
+    public boolean adicionarCoordenacao(Coordenacao coordenacao) {
+        if (coordenacao == null) {
+            throw new IllegalArgumentException("Coordenação não pode ser nula");
+        }
+
+        int idCoordenacao = coordenacao.getIdCoordenacao();
+
+        if (coordenacoes.containsKey(idCoordenacao)) {
+            return false;
+        }
+
+        coordenacoes.put(idCoordenacao, coordenacao);
+
+        return true;
+    }
+
+    public boolean removerCoordenacao(int idCoordenacao) {
+        return coordenacoes.remove(idCoordenacao) != null;
+    }
+
+    public Coordenacao buscarCoordenacaoPorId(int idCoordenacao) {
+        return coordenacoes.get(idCoordenacao);
+    }
+
+    public boolean adicionarCurso(Curso curso) {
+        if (curso == null) {
+            throw new IllegalArgumentException("Curso não pode ser nulo");
+        }
+
+        int idCurso = curso.getCodigoCurso();
+
+        if (cursos.containsKey(idCurso)) {
+            return false;
+        }
+
+        cursos.put(idCurso, curso);
+
+        return true;
+    }
+
+    public boolean removerCurso(int idCurso) {
+        return cursos.remove(idCurso) != null;
+    }
+
+    public Curso buscarCursoPorId(int idCurso) {
+        return cursos.get(idCurso);
+    }
+
+    public boolean adicionarDisciplina(Disciplina disciplina) {
+        if (disciplina == null) {
+            throw new IllegalArgumentException("Disciplina não pode ser nula");
+        }
+
+        int id = disciplina.getIdDisciplina();
+
+        if (disciplinas.containsKey(id)) {
+            return false;
+        }
+
+        disciplinas.put(id, disciplina);
+
+        return true;
+    }
+
+    public boolean removerDisciplina(int idDisciplina) {
+        return disciplinas.remove(idDisciplina) != null;
+    }
+
+    public Disciplina buscarDisciplinaPorId(int idDisciplina) {
+        return disciplinas.get(idDisciplina);
+    }
+
+    public boolean adicionarPlanoDeEnsino(PlanoDeEnsino plano) {
+        if (plano == null) {
+            throw new IllegalArgumentException("Plano de Ensino não pode ser nulo");
+        }
+
+        int id = plano.getIdPlanoDeEnsino();
+
+        if (planosDeEnsino.containsKey(id)) {
+            return false;
+        }
+
+        planosDeEnsino.put(id, plano);
+
+        return true;
+    }
+
+    public boolean removerPlanoDeEnsino(int idPlano) {
+        return planosDeEnsino.remove(idPlano) != null;
+    }
+
+    public PlanoDeEnsino buscarPlanoDeEnsinoPorId(int idPlano) {
+        return planosDeEnsino.get(idPlano);
+    }
+
+    public boolean adicionarProfessor(Professor professor) {
+        if (professor == null) {
+            throw new IllegalArgumentException("Professor não pode ser nulo");
+        }
+
+        int id = professor.getIdProfessor();
+
+        if (professores.containsKey(id)) {
+            return false;
+        }
+
+        professores.put(id, professor);
+
+        return true;
+    }
+
+    public boolean removerProfessor(int idProfessor) {
+        return professores.remove(idProfessor) != null;
+    }
+
+    public Professor buscarProfessorPorId(int idProfessor) {
+        return professores.get(idProfessor);
+    }
+
+    public boolean adicionarUnidadeAcademica(UnidadeAcademica unidade) {
+        if (unidade == null) {
+            throw new IllegalArgumentException("Unidade Acadêmica não pode ser nula");
+        }
+
+        int id = unidade.getIdUnidadeAcademica();
+
+        if (unidadesAcademicas.containsKey(id)) {
+            return false;
+        }
+
+        unidadesAcademicas.put(id, unidade);
+
+        return true;
+    }
+
+    public boolean removerUnidadeAcademica(int idUnidade) {
+        return unidadesAcademicas.remove(idUnidade) != null;
+    }
+
+    public UnidadeAcademica buscarUnidadeAcademicaPorId(int idUnidade) {
+        return unidadesAcademicas.get(idUnidade);
     }
 
     public void notificarObservers() {

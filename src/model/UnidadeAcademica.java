71dfd1cb4a9aa48b26d00cc177b2c2bf3e513834
Bernardo Euler <bernardoeulersantos@gmail.com>
2025-07-11 00:31:47 -0300

    feat: Adicionar construtores, getters e setters à classe `UnidadeAcademica`

--- src/model/UnidadeAcademica.java
+++ src/model/UnidadeAcademica.java
@@ -4,4 +4,38 @@ public class UnidadeAcademica implements Universidade {
     private int idUnidadeAcademica;
     private String nome;
     private String endereco;
+
+    public UnidadeAcademica() {
+        super();
+    }
+
+    public UnidadeAcademica(int idUnidadeAcademica, String nome, String endereco) {
+        this.idUnidadeAcademica = idUnidadeAcademica;
+        this.nome = nome;
+        this.endereco = endereco;
+    }
+
+    public int getIdUnidadeAcademica() {
+        return idUnidadeAcademica;
+    }
+
+    public void setIdUnidadeAcademica(int idUnidadeAcademica) {
+        this.idUnidadeAcademica = idUnidadeAcademica;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
 }
